e5462233b2cc0fa762c74aa044df9ddcc6aea7ff
Stephanie Martinez <stephaniemartinez@Stephanies-Air.lan>
2024-04-06 00:45:25 -0400

    Added composable packages

--- app/src/main/java/com/example/cs4520_twitter/MainActivity.kt
+++ app/src/main/java/com/example/cs4520_twitter/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     private val blue :  androidx.compose.ui.graphics.Color = Color(0xFF9BAAF8) // 0xFF9BAAF8
@@ -69,14 +68,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                }
-            }
         }
     }
 }
