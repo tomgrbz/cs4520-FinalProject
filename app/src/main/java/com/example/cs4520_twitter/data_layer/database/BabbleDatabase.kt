0e5bc4861b5d69bee7138dcf6e87517eeecf47d9
Thomas Grbic <tomgrbz@gmail.com>
2024-04-08 01:13:47 -0400

    Add companion obj to get singleton instance of DB

--- app/src/main/java/com/example/cs4520_twitter/data_layer/database/BabbleDatabase.kt
+++ app/src/main/java/com/example/cs4520_twitter/data_layer/database/BabbleDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.cs4520_twitter.data_layer.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.cs4520_twitter.utils.UserEntityTypeConverter
@@ -13,4 +15,28 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun userProfileDao(): UserProfileDao
 
     abstract fun babDao(): BabDao
+
+    companion object {
+
+        @Volatile
+        private var dbInstance: AppDatabase? = null
+
+
+        fun instance(context: Context): AppDatabase {
+            synchronized(this) {
+                var instance = dbInstance
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context,
+                        AppDatabase::class.java,
+                        "babbleDB"
+                    ).build()
+                    dbInstance = instance
+
+                }
+                return instance
+            }
+        }
+    }
 }
