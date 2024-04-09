1d31b3f3d7498afa1728e0ecc2b801ff7e7a170d
tomgrbz <62816199+tomgrbz@users.noreply.github.com>
2024-04-08 22:42:26 -0400

    Data access objects (#1)
    * add room dependencies and db tables for babs, users, and user profiles
    
    * Add companion obj to get singleton instance of DB
    
    * Docs and change table names to *Entity
    
    * Added methods to update user profile description, and following list, and delete bab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
 }
 
 android {
@@ -62,6 +63,10 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.room:room-runtime:2.5.0")
+    kapt("androidx.room:room-compiler:2.5.0")
+    implementation("androidx.room:room-ktx:2.5.0")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
@@ -69,5 +74,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
 }
