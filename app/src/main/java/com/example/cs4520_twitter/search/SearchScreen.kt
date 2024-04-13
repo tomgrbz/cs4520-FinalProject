fd98dfbe424d805bbf2ce16cd714f7bb01a09f43
allychoung <allychoung@gmail.com>
2024-04-13 14:16:30 -0400

    add styling

--- app/src/main/java/com/example/cs4520_twitter/search/SearchScreen.kt
+++ app/src/main/java/com/example/cs4520_twitter/search/SearchScreen.kt
@@ -1,11 +1,18 @@
 package com.example.cs4520_twitter.search
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -19,9 +26,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.cs4520_twitter.R
 
@@ -30,41 +40,63 @@ import com.example.cs4520_twitter.R
 fun SearchScreen() {
     val viewModel: SearchScreenViewModel = viewModel()
     var searchInput by remember { mutableStateOf("") }
+    val configuration = LocalConfiguration.current
+    val maxWidth = configuration.screenWidthDp
 
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.White),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        TextField(
-            value = searchInput,
-            placeholder = { Text("Search Babble") },
-            onValueChange = { searchInput = it },
-            singleLine = true,
-            colors = TextFieldDefaults.colors(
-                unfocusedContainerColor = Color.White,
-                unfocusedIndicatorColor = Color.Transparent,
-                focusedContainerColor = Color.White,
-                focusedIndicatorColor = Color.Transparent,
-                focusedTextColor = Color.Black,
-            ),
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        IconButton(
-            onClick = {  },
+    Column {
+        Text(
+            "Search",
             modifier = Modifier
-                .size(40.dp)
-                .background(Color(0xFFB9C1F1), CircleShape),
-            content = {
-                Icon(
-                    painter = painterResource(id = R.drawable.search_icon),
-                    contentDescription = "Search button icon",
-                    Modifier.size(20.dp)
-                )
-            }
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(10.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
         )
-        // TODO() add post components here
-        // for (Post p in viewModel.results) ...
-    }
+
+        Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .border(BorderStroke(1.dp, Color(0xFF9BAAF8))),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextField(
+                    value = searchInput,
+                    placeholder = { Text("Search Babble") },
+                    onValueChange = { searchInput = it },
+                    singleLine = true,
+                    modifier = Modifier.width((maxWidth * .8).dp),
+                    colors = TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color.White,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedContainerColor = Color.White,
+                        focusedIndicatorColor = Color.Transparent,
+                        focusedTextColor = Color.Black,
+                    ),
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                IconButton(
+                    onClick = { },
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(40.dp)
+                        .background(Color(0xFFB9C1F1), RoundedCornerShape(15.dp)),
+                    content = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.search_icon),
+                            contentDescription = "Search button icon",
+                            Modifier.size(20.dp)
+                        )
+                    }
+                )
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(10.dp),
+                    verticalArrangement = Arrangement.spacedBy(0.dp),
+                ) {
+
+                }
+            }
+        }
 }
