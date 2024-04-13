8494bc32e2ffebf2fa464b7d3cfc42e41120fd19
allychoung <allychoung@gmail.com>
2024-04-13 13:51:30 -0400

    style following cards

--- app/src/main/java/com/example/cs4520_twitter/following/FollowingScreen.kt
+++ app/src/main/java/com/example/cs4520_twitter/following/FollowingScreen.kt
@@ -1,18 +1,14 @@
 package com.example.cs4520_twitter.following
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CornerSize
@@ -24,7 +20,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults.indicatorLine
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
@@ -34,10 +29,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -47,8 +40,8 @@ fun FollowingScreen() {
     val viewModel: FollowingScreenViewModel = viewModel()
     val configuration = LocalConfiguration.current
     val maxHeight = configuration.screenHeightDp
-    val maxWidth = configuration.screenWidthDp
 
+    val blue :  Color = Color(0xFF9BAAF8)
     Scaffold(
         containerColor = Color.Transparent,
         topBar = { // Contain the feed title
@@ -88,22 +81,52 @@ fun FollowingScreen() {
                         .padding(
                             PaddingValues(10.dp, 10.dp, 10.dp, 0.dp)
                         )
-                        .height(80.dp),
+                        .height((maxHeight * 0.1).dp),
                     colors = CardDefaults.cardColors(
                         containerColor = Color.White
                     ),
                     shape = RoundedCornerShape(corner = CornerSize(15.dp)),
                 ) {
-                    Row(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(20.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+//                        GlideImage( // this is the icon image
+//                            model = dummyImageURL,
+//                            contentScale = ContentScale.Crop,
+//                            loading = placeholder(ColorPainter(Color.White)),
+//                            failure = placeholder(ColorPainter(Color.White)),
+//                            contentDescription = "",
+//                            modifier = Modifier
+//                                .size(60.dp) // hardcoded icon dim.'s, etc.
+//                                .shadow(
+//                                    elevation = 5.dp,
+//                                    shape = RoundedCornerShape(10.dp)
+//                                )
+//                                .border(
+//                                    width = 1.dp,
+//                                    color = blue,
+//                                    shape = RoundedCornerShape(10.dp)
+//                                )
+//                                .constrainAs(userIcon) { todo("remove")
+//                                    // halfway icon height
+//                                    top.linkTo(parent.top, margin = (maxHeight * 0.1 - 65).dp)
+//                                    // Place icon midway the screen's width
+//                                    absoluteLeft.linkTo(
+//                                        parent.absoluteLeft,
+//                                        margin = (10).dp
+//                                    )
+//                                })
+
                         Text(text = followedUser.username)
                         Spacer(modifier = Modifier.weight(1f))
                         Button(
                             onClick = { viewModel.unfollow(followedUser.id) },
                             contentPadding = PaddingValues(0.dp),
-                            border = BorderStroke(2.dp, Color.Blue
-                            ), // todo swap with theme blue on merge
+                            border = BorderStroke(2.dp, blue
+                            ),
                             colors = ButtonDefaults.buttonColors(
-                                contentColor = Color.Blue,
+                                contentColor = blue,
                                 containerColor = Color.White),
                             modifier = Modifier
                                 .height(35.dp)
