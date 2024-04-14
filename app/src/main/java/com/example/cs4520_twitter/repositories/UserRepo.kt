6ad7373e0893e50468257fe22257b3775ae9ac94
Oceane <kenfacktonfack.o@northeastern.edu>
2024-04-14 16:54:57 -0400

    made user repo

--- app/src/main/java/com/example/cs4520_twitter/repositories/UserRepo.kt
+++ app/src/main/java/com/example/cs4520_twitter/repositories/UserRepo.kt
@@ -1,5 +1,22 @@
 package com.example.cs4520_twitter.repositories
 
+import android.content.Context
+import com.example.cs4520_twitter.data_layer.database.AppDatabase
 import com.example.cs4520_twitter.data_layer.database.UserEntity
 
-internal class UserRepo(userEntity: UserEntity?)
+
+interface UserRepository {
+    suspend fun getUser(userID: String): UserEntity
+}
+
+/**
+ * Implementation of Repository that fetch Users
+ */
+class UserRepo(private val db : AppDatabase) : UserRepository {
+
+    /** Fetches list of User from Local database*/
+      override suspend fun getUser(userID: String) : UserEntity {
+         val userDao = db.userDao()
+         return userDao.getByUserID(userID)
+     }
+}
