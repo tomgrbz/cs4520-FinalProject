ed4815a0637fde15437ef1b70e821c9c444b8b5c
Thomas Grbic <tomgrbz@gmail.com>
2024-04-14 17:31:52 -0400

    deduplicate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
