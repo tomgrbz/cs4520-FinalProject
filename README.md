e03ad4cad338cac8fa8865a67760ea2a66884403
Stephanie Martinez <stephaniemartinez@Stephanies-Air.lan>
2024-04-16 10:18:14 -0400

    Added registration functionality, see readme

--- README.md
+++ README.md
@@ -7,7 +7,14 @@ Some comments
 - I think there may be an issue with the "testuser" user's userID (it's 123). I get an error at line "UUID.fromString(LoggedInUser.loggedInUserId)" saying that testuser has an invalid UUID string after logging in and navigating to the profile.
 - But I can log in with users like 'ben10' and '#1pokemonfan' and I can navigate to profile which loads correctly (aside from the banner username, but I need an api method to retrieve a user).
 - I also think there may be an issue with the image URL's from the API's users, it gives me res/bear when I obtain it
+- I also added registration functionality. If you click on the text at the bottom, it will change to login button
+- to a register one, and a user can successfully register if both text fields are not blank. I used the signup api for this
+- I tested it myself with username and password "foo". Then if you navigate to the profile screen, there is a blank profile.
 
 ![Alt text](example_login.png)
 
 ![Alt text](uuid_error_for_testuser.png)
+
+![Alt text](registration.png)
+
+![Alt text](new_registered_user.png)
--- app/src/main/java/com/example/cs4520_twitter/vms/LoginViewModel.kt
+++ app/src/main/java/com/example/cs4520_twitter/vms/LoginViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.cs4520_twitter.vms
 
 import android.util.Log
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -8,21 +10,21 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.cs4520_twitter.app_state.LoggedInUser
 import com.example.cs4520_twitter.application.BabbleApplication
 import com.example.cs4520_twitter.data_layer.api.LoginApi
+import com.example.cs4520_twitter.data_layer.api.SignupApi
 import com.example.cs4520_twitter.data_layer.api.models.CredentialsPostRequest
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class LoginViewModel(val loginApi: LoginApi) : ViewModel() {
+class LoginViewModel(private val loginApi: LoginApi,
+                     private val signupApi : SignupApi) : ViewModel() {
 
     private val _isLoading = MutableStateFlow<Boolean>(true);
-
     val isLoading: StateFlow<Boolean> get() = _isLoading.asStateFlow()
 
-
     fun login(username: String, password: String) {
-
+        // validate user credentials
         _isLoading.value = true
 
         viewModelScope.launch {
@@ -32,7 +34,27 @@ class LoginViewModel(val loginApi: LoginApi) : ViewModel() {
                 LoggedInUser.loggedInUserId = resp.user.userID
             } catch (e: Exception) {
                 Log.e("LoginViewModel", "Failed to fetch resp due to $e")
+
             }
+        }
+    }
+
+    fun signUp(username: String, password: String) {
+        // register user, unless one of the username or password is blank
+        _isLoading.value = true
+
+        if (username.isNotBlank() and password.isNotBlank()) { // valid fields
+            viewModelScope.launch {
+                try {
+                    val resp = signupApi.signup(CredentialsPostRequest(username, password))
+                    Log.i("LoginViewModel", "Signed up in $resp")
+                    LoggedInUser.loggedInUserId = resp.user.userID // Automatically log them in?
+                } catch (e: Exception) {
+                    Log.e("LoginViewModel", "Failed to sign up due to $e")
+                }
+            }
+        } else {
+            Log.e("LoginViewModel", "Failed to sign up due to blank fields.")
         }
     }
 
@@ -43,7 +65,8 @@ class LoginViewModel(val loginApi: LoginApi) : ViewModel() {
 
                 val application =
                     checkNotNull(extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY])
-                return LoginViewModel((application as BabbleApplication).appContainer.loginApiService) as T
+                return LoginViewModel((application as BabbleApplication).appContainer.loginApiService,
+                    (application as BabbleApplication).appContainer.signupApiService) as T
             }
         }
     }
