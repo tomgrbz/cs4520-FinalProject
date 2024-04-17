edd1f92bbddbef21163601fda8cf2382f1be41c3
Oceane <kenfacktonfack.o@northeastern.edu>
2024-04-17 11:01:26 -0400

    added Generic response to profile api

--- app/src/main/java/com/example/cs4520_twitter/data_layer/api/ProfilesApi.kt
+++ app/src/main/java/com/example/cs4520_twitter/data_layer/api/ProfilesApi.kt
@@ -1,5 +1,7 @@
 package com.example.cs4520_twitter.data_layer.api
 
+import com.example.cs4520_twitter.data_layer.database.UserProfileEntity
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import java.util.UUID
@@ -7,5 +9,5 @@ import java.util.UUID
 interface ProfilesApi {
 
     @GET(Api.PROFILES_ENDPOINT + "/{userID}")
-    suspend fun getUserProfile(@Path("userID") userID: UUID)
+    suspend fun getUserProfile(@Path("userID") userID: UUID) : Response<UserProfileEntity>
 }
