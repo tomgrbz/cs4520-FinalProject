3d831a875949a6aa4460a7d5920562f5f06a0533
Oceane K <133834859+OceKen@users.noreply.github.com>
2024-04-17 20:11:01 -0400

    Data repositories Fixed Changes (#23)
    * bab repo
    
    * made userprofile repo
    
    * made user repo
    
    * Update BabRepo.kt, fixed naming
    
    * still working
    
    * finished User Profile Repository
    
    * added Generic response to profile api
    
    * finished UserRepo
    
    * finished bab repo
    
    * Fixed changes
    
    * Updated to have Get Profile response
    
    * fixed api and local call

--- app/src/main/java/com/example/cs4520_twitter/repositories/UserRepo.kt
+++ app/src/main/java/com/example/cs4520_twitter/repositories/UserRepo.kt
@@ -1,22 +1,44 @@
 package com.example.cs4520_twitter.repositories
 
-import android.content.Context
+import com.example.cs4520_twitter.data_layer.api.UsersApi
+import com.example.cs4520_twitter.data_layer.api.models.RandomBabsResponse
+import com.example.cs4520_twitter.data_layer.api.models.UsersFollowingResponse
 import com.example.cs4520_twitter.data_layer.database.AppDatabase
 import com.example.cs4520_twitter.data_layer.database.UserEntity
+import retrofit2.http.Path
+import java.util.UUID
 
 
 interface UserRepository {
     suspend fun getUser(userID: String): UserEntity
+    suspend fun insertUser(user: UserEntity)
+
+    suspend fun getUsersFollowing(userID: UUID): UsersFollowingResponse
+
+    suspend fun getUserBabs(userID: UUID): RandomBabsResponse
 }
 
 /**
  * Implementation of Repository that fetch Users
  */
-class UserRepo(private val db : AppDatabase) : UserRepository {
+class UserRepo(private val db : AppDatabase, private val api : UsersApi) : UserRepository {
+    private val userDao = db.userDao()
 
     /** Fetches list of User from Local database*/
       override suspend fun getUser(userID: String) : UserEntity {
-         val userDao = db.userDao()
          return userDao.getByUserID(userID)
      }
+
+    override suspend fun insertUser(user: UserEntity) {
+        userDao.insert(user)
+    }
+
+    override suspend fun getUsersFollowing(userID: UUID): UsersFollowingResponse {
+        return api.getUsersFollowing(userID)
+    }
+
+    override suspend fun getUserBabs(userID: UUID): RandomBabsResponse {
+        return api.getUserBabs(userID)
+    }
+
 }
